9a0a036eda532beaad772d6323ab49f3f4d714c6
Brittany Kerr and Rena Rallis <rena@>
2017-06-01 13:28:32 -0700

    add form values, submit button

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("form#soulmate").submit(function(event) {
+    event.preventDefault();
+    var pet = $(select#pet).val());
+    var romantic_getaway =$(select#romantic_getaway).val());
+    var residence = $(select#residence).val());
+  });
+});
